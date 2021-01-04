6faf1668a72d69077ae6e74217393284c91cc2e9
gavin4396 <76945571+gavin4396@users.noreply.github.com>
2021-01-05 03:51:48 +0800

    Add files via upload

--- classifying_news_pro3.py
+++ classifying_news_pro3.py
@@ -48,6 +48,13 @@ model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accu
 x_val=x_train[:1000]
 rest_x_train=x_train[1000:]
 
+
+
+
+
+
+
+
 y_val=y_train[:1000]
 rest_y_train=y_train[1000:]
 
@@ -69,6 +76,66 @@ plt.xlabel('epochs')
 plt.ylabel('loss')
 plt.show()
 
+acc=process.history['accuracy']
+val_acc=process.history['val_accuracy']
+plt.plot(epochs,acc,'bo',label='train accuracy')
+plt.plot(epochs,val_acc,'b',label='validation acc')
+plt.title('training and validation acc')
+plt.xlabel('epochs')
+plt.ylabel('acc')
+plt.show()
+########################plot clear##########################
+
+plt.clf()
+
+#####################evaluation/testing part################
+results=model.evaluate(x_test,y_test)
+############################################################
+
+####################prediction new data#####################
+x_test=x_test[:500]
+prediction=model.predict(x_test)
+prediction.shape
+prediction[1]
+np.sum(prediction[1])#############check all possibility summation=1
+np.argmax(prediction[1])
+
+
+"""
+Wrapping up
+
+Here's what you should take away from this example:
+
+   * If you are trying to classify data points between N classes, your network should end with a Dense layer of size N.
+    *In a single-label, multi-class classification problem, your network should end with a softmax activation, so that it will output a probability distribution over the N output classes.
+    *Categorical crossentropy is almost always the loss function you should use for such problems. It minimizes the distance between the probability distributions output by the network, and the true distribution of the targets.
+    *There are two ways to handle labels in multi-class classification: Encoding the labels via "categorical encoding" (also known as "one-hot encoding") and using categorical_crossentropy as your loss function. Encoding the labels as integers and using the sparse_categorical_crossentropy loss function.
+    If you need to classify data into a large number of categories, then you should avoid creating information bottlenecks in your network by having intermediate layers that are too small.
+"""
+####################################this is integers and using the sparse_categorical_crossentropy loss function######################
+model=models.Sequential()
+model.add(layers.Dense(64,activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(46,activation='softmax'))
+# model compile
+model.compile(optimizer='rmsprop',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+######################################################
+
+x_val=x_train[:1000]
+rest_x_train=x_train[1000:]
+
+
+
+
+
+
+
+
+y_val=train_labels[:1000]
+rest_y_train=train_labels[1000:]
+
+############### train the model and validation#########
+process=model.fit(rest_x_train,rest_y_train,epochs=10,batch_size=512,validation_data=(x_val,y_val))
 
 
 
